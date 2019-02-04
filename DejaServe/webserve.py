5241d3616aabe6a301c66b87567dde2fffe7e1ba
Akshay Brijpuria <akshay.brijpuria@gmail.com>
2019-02-04 17:13:45 +0530

    Fin

--- DejaServe/webserve.py
+++ DejaServe/webserve.py
@@ -5,8 +5,11 @@ import random
 import cgi 
 import json
 import os
+import datetime
+import operator
 import pandas as pd
 from matplotlib import pyplot as plt
+
 port_num=8080
 hostName="localhost"
 
@@ -25,37 +28,57 @@ class DejaServer(BaseHTTPRequestHandler):
         dt=json.loads(data)
         return dt
 
+    def resp_calc(self, s):
+            return s.replace('sec','')
+    
+    def last_hour(self, log_date):
+            current=datetime.datetime.today()
+            last_hour=current-datetime.timedelta(hours=1)
+            return operator.ge(log_date,last_hour)
+
+    def last_minute(self, log_minute):
+            current=datetime.datetime.today()
+            last_minute=current-datetime.timedelta(minutes=1)
+            return operator.ge(log_minute,last_minute)
+    
     def add_headers(self):
-        #self.send_response(200)
         self.send_header('Content-Type','application/json')
         self.send_header('X-Frame-Options','DENY')
         self.send_header('Cache-Control','no-cache')
     
     def log_requests(self,data):
         with open('temp.csv','a') as w:
-#            import pdb;pdb.set_trace()
-           # print("{} {} {}".format(data['path'],data['Method'],data['ReceivedTime'],data['Processing Time'],data['query']))
             w.write("{},{},{},{}\n".format(data['path'],data['Method'],data['ReceivedTime'],data['Processing Time'],data['query']))
    
     def get_stats(self):
         df=pd.read_csv('temp.csv')
-      #  import pdb;pdb.set_trace()
+        column=['path','method','receiving_date','processing_time']
+        df.columns=column
+        current_date=datetime.datetime.now()
+        df['receiving_date'] = pd.to_datetime(df['receiving_date'])
+        process_time=list(map(self.resp_calc,df['processing_time']))
+        df['processing_time']=pd.Series(map(float,process_time))
+        avg_response=sum(df['processing_time'])/len(df['processing_time'])
+        _hours=list(map(self.last_hour,df['receiving_date']))
+        _minutes=list(map(self.last_minute,df['receiving_date']))
+        total_request=len(df['method']) 
+        total_get_req=len(df[df['method'] == 'GET'])
+        total_post_req=len(df[df['method'] == 'POST'])
+        return '{"Total" : "%s", "GET" : "%s", "POST" : "%s", "Average Response" : "%s", "Last Hour" : "%s", "Last Minute" : "%s"}' % (total_request,total_get_req,total_post_req,round(avg_response,2),len(df[_hours]),len(df[_minutes]))
+
         
     def handler404(self):
         self.send_response(404)
-        #print('s')
-        #self.send_header('Content-Type')
         self.add_headers()
         self.end_headers()
         data=self.add_response()
         self.wfile.write(bytes(str(data), 'utf-8'))
-        #self.wfile.write(b'''<html>Error!!!</html>''')
+        self.log_requests(data)
     
     def do_GET(self):
         if self.path == '/process':
             self.send_response(200)
             self.add_headers()
-            #self.send_header('Content-Type','text/html')
             self.end_headers()
             data = self.add_response()
             self.wfile.write(bytes(str(data), 'utf-8'))
@@ -65,7 +88,8 @@ class DejaServer(BaseHTTPRequestHandler):
             self.send_response(200)
             self.add_headers()
             self.end_headers()
-            self.get_stats()
+            time.sleep(random.random() * 10) 
+            data = self.get_stats()
             self.wfile.write(bytes(str(data), 'utf-8'))
         else:
             self.handler404()
@@ -76,7 +100,6 @@ class DejaServer(BaseHTTPRequestHandler):
         self.end_headers()
         form = cgi.FieldStorage(fp=self.rfile,headers=self.headers,environ={'REQUEST_METHOD': 'POST'})
         query = form.getvalue("data")
- #       self.wfile.write(b'<html>POST_req</html>')
         data=self.add_response(query)
      #   dt=json.loads(data)
     #iprint(dt)
@@ -84,7 +107,25 @@ class DejaServer(BaseHTTPRequestHandler):
         self.log_requests(data)
 
     def do_PUT(self):
-        self.wfile.write(b'PUT_req')
+        self.send_response(200)
+        self.add_headers()
+        self.end_headers()
+        form = cgi.FieldStorage(fp=self.rfile,headers=self.headers,environ={'REQUEST_METHOD': 'PUT'}) 
+        query = form.getvalue("data")
+        data = self.add_response(query)
+        self.wfile.write(bytes(str(data), 'utf-8' ))
+        self.log_requests(data)
+
+
+    def do_DELETE(self):
+        self.send_response(301)
+        self.add_headers()
+        self.end_headers()
+        form = cgi.FieldStorage(fp=self.rfile,headers=self.headers,environ={'REQUEST_METHOD': 'DELETE'})
+        query = form.getvalue('data')
+        data = self.add_response(query)
+        self.wfile.write(bytes(str(data), 'utf-8' ))
+        self.log_requests(data)
 
 if __name__=='__main__':
     #dejaSer = DejaServer()
